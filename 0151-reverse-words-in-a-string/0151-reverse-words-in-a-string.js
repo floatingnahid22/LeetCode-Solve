69270e7f903ea8db03ed4f724dd9c58effaa7eda
Nahid Hossain <81576753+floatingnahid22@users.noreply.github.com>
2023-09-15 17:35:38 +0600

    Time: 53 ms (70.64%), Space: 43.4 MB (91.02%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -7,7 +7,11 @@ var reverseWords = function(s) {
     let res = "";
     for(let i=str.length-1; i>=0; i--){
         if(str[i]){
-            res += (res ? " " : "") + str[i];
+            if(res){
+                res = res + " " + str[i];
+            } else {
+                res = res + "" + str[i];
+            }
         }
     }
     return res;
